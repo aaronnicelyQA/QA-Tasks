fdd7b115df3ca25e881ef5b5fccd09f550886e3d
Luk14 <lukaszplayz121@gmail.com>
2020-12-10 10:38:51 +0000

    Added Factorial (Task 1)

--- src/com/qa/Factorials.java
+++ src/com/qa/Factorials.java
@@ -1,5 +1,39 @@
 package com.qa;
 
 public class Factorials {
+	
+	private int number;
+	
+	public Factorials(int number)
+	{
+		this.number = number;
+	}
+	
+	public int getFactorial()
+	{
+		int i = 2;
+		while (true)
+		{
+			if(getNumber()==1)
+			{
+				return i-1;
+			}
+			if(getNumber()/2 < 1)
+			{
+				return 0;
+			}
+			setNumber(getNumber()/i);
+			i++;
+		}
+	}
+	
+    public int getNumber()
+    {
+        return number;
+    }
 
+    public void setNumber(int num)
+    {
+        this.number = num;
+    }
 }
--- src/com/qa/Runner.java
+++ src/com/qa/Runner.java
@@ -1,5 +1,32 @@
 package com.qa;
 
 public class Runner {
+	
+	public static void main(String[] args)
+	{
+		printResultsForTask1();
+	}
+	
+	/*
+	 * Factorial (Task 1)
+	 */
+	private static void printResultsForTask1()
+	{
+		Factorials factorial = new Factorials(120);
+		int result = factorial.getFactorial();
+		System.out.println("Answer: " + (result==0 ? "Non" : result));
+		factorial.setNumber(3628800);
+		int result2 = factorial.getFactorial();
+		System.out.println("Answer: " + (result2==0 ? "Non" : result2));
+		factorial.setNumber(479001600);
+		int result3 = factorial.getFactorial();
+		System.out.println("Answer: " + (result3==0 ? "Non" : result3));
+		factorial.setNumber(6);
+		int result4 = factorial.getFactorial();
+		System.out.println("Answer: " + (result4==0 ? "Non" : result4));
+		factorial.setNumber(18);
+		int result5 = factorial.getFactorial();
+		System.out.println("Answer: " + (result5==0 ? "Non" : result5));
+	}
 
 }
